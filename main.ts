c99a3af6ff67e8b6f6a49088f4deacf5842885a7
Taha Ansari <t.ansari@clerkswell.com>
2023-02-16 17:55:11 +0530

    wip

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 export {};
-let a = "Welcome Back";
-console.log(a);
+// let a = "Welcome Back";
+// console.log(a);
+
+let x = 10;
+const y = 20;
+
+// let x = 40;
+
+var a = "hello";
+var a = "world";
+
+const b = "hi";
+
+let isBigeneer: Boolean = false;
+let total: Number = 1;
+let name: String = "Taha Ansari";
+let sentence: String = `My name is ${name} 
+and i am new to typescript`;
+
+// total = true;
+
+let n: null = null;
+let u: undefined = undefined;
+
+let newTotal: boolean = null;
